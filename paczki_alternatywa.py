6857793acf6bfaf418c2474c5c0bd80d9bd0a4a2
Filip Andrzejczyk <filipandrzejczyk123@gmail.com>
2024-02-08 20:46:47 +0100

    poprawiono

--- paczki_alternatywa.py
+++ paczki_alternatywa.py
@@ -1,31 +1,28 @@
 ilosc_elementow = int(input("Ile elementow chcesz wyslac ?"))
-waga = 0
-ilosc_paczek = 0
-
+waga_total = 0
+waga_paczki = 0
+ilosc_paczek = 1
 
 for element in range(ilosc_elementow):
     waga_elementu = float(input("Podaj wage elementu"))
-    if waga_elementu >= 1 and waga_elementu <= 10: #Jest to troche bez sensu bo mozemy przecież wysłać 1 element, ktory ma 20 kg i co za tym idzie mieści się w 1 paczce.
-        waga = waga + waga_elementu
-        ilosc_paczek = 1
-        if waga > 20:
-            ilosc_paczek += 1
-        else: continue
-    else:
-        print("Waga elementu jest zbyt duża lub zbyt mała proces zostanie przerwany a dotychczasowe POPRAWNIE NADANE paczki wyslane")
+    if waga_elementu < 1 or waga_elementu > 10:
+        print("Element zbyt duzy lub zbyt maly")
         break
+    else:
+        waga_paczki += waga_elementu
+        waga_total += waga_elementu
 
-suma_pustych_kilogramow = ilosc_paczek * 20 - waga
-
-print(f'Wyslano {ilosc_paczek} paczke/paczki \n Wyslano {waga} kg \n Suma pustych kilogramow: {suma_pustych_kilogramow}') 
-if ilosc_paczek > 1:
-    print(f'Najwiecej pustych kilogramow ma paczka nr {ilosc_paczek}, {20 - suma_pustych_kilogramow} kg')
-else: 
-    print(f'Najwiecej pustych kilogramow ma paczka nr {ilosc_paczek}, {20 - waga} kg')
-
+    if waga_paczki + waga_elementu > 20:
+        waga_paczki = waga_elementu
+        ilosc_paczek += 1
 
 
-
-
-
+if waga_total <= 20:
+    ilosc_paczek = 1
+    waga_paczki = waga_total
+suma_pustych_kilogramow = ilosc_paczek * 20 - waga_total
+print(f'Wyslano {ilosc_paczek} paczke/paczki')
+print(f'Wyslano {waga_total} kg')
+print(f'Suma pustych kilogramow: {suma_pustych_kilogramow} kg')
+print(f'Najwiecej pustych kilogramow miala paczka nr {ilosc_paczek}, {20 - waga_paczki} kg')
 
